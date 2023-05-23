09eba1970629805e9bca325f7cac2c7f34ba18ce
Fabrício Ferreira <fabriciof481@gmail.com>
2023-05-22 22:51:45 -0300

    Organizando e Adicionando exercícios

--- 01-referencia/00-livros/01-pense-em-python/0003-velocidade-m-mi/main.py
+++ 01-referencia/00-livros/01-pense-em-python/0003-velocidade-m-mi/main.py
@@ -2,4 +2,5 @@ print("Considerando que você correu 10km em 42 minutos e 42 segundos")
 print("Qual é a sua velocidade média em milhas por hora?")
 print(10/1.61)
 print((10/1.61)/(42/60+(42/60)/60))
+#teste boy
 print("mi/h")
